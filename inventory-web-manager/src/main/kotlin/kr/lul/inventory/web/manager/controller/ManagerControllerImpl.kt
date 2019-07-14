634f7f1660627dac7f8f72c9d050f5c05ccb2299
Just Burrow <just.burrow@lul.kr>
2019-07-14 23:24:01 +0900

    ADD : `Manager` 가입시 검증 에러 메시지를 추가.

--- inventory-web-manager/src/main/kotlin/kr/lul/inventory/web/manager/controller/ManagerControllerImpl.kt
+++ inventory-web-manager/src/main/kotlin/kr/lul/inventory/web/manager/controller/ManagerControllerImpl.kt
@@ -2,6 +2,7 @@ package kr.lul.inventory.web.manager.controller
 
 import kr.lul.inventory.business.borderline.ManagerBorderline
 import kr.lul.inventory.business.borderline.cmd.CreateManagerCmd
+import kr.lul.inventory.design.domain.AttributeValidationException
 import kr.lul.inventory.web.manager.controller.request.CreateManagerReq
 import kr.lul.inventory.web.manager.mapping.ManagerMvc.M
 import kr.lul.inventory.web.manager.mapping.ManagerMvc.V
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.ModelAttribute
 import javax.validation.Valid
 
@@ -20,10 +22,43 @@ internal class ManagerControllerImpl : ManagerController {
     @Autowired
     private lateinit var managerBorderline: ManagerBorderline
 
+    private fun validate(req: CreateManagerReq, result: BindingResult) {
+        if (null != req.secret && !req.secret.equals(req.confirm)) {
+            result.addError(FieldError(M.CREATE_MANAGER_REQ, "confirm", null,
+                    false, arrayOf("err.manager.create.confirm-not-match"), arrayOf(),
+                    "secret confirm does not match."))
+        }
+    }
+
+    private fun doCreateForm(model: Model): String {
+        if (!model.containsAttribute(M.CREATE_MANAGER_REQ))
+            model.addAttribute(M.CREATE_MANAGER_REQ, CreateManagerReq())
+
+        return V.CREATE_FORM
+    }
+
+    private fun doCreate(req: CreateManagerReq, result: BindingResult, model: Model): String {
+        return try {
+            managerBorderline.create(CreateManagerCmd(
+                    req.email!!,
+                    req.name!!,
+                    req.secret!!
+            ))
+
+            "redirect:/"
+        } catch (e: AttributeValidationException) {
+            result.addError(FieldError(M.CREATE_MANAGER_REQ, e.attribute, e.value, false,
+                    arrayOf(), arrayOf(), e.message))
+            doCreateForm(model)
+        }
+    }
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // kr.lul.inventory.web.manager.controller.ManagerController
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     override fun createForm(model: Model): String {
+        val template = doCreateForm(model)
+
         model.addAttribute(M.CREATE_MANAGER_REQ, CreateManagerReq())
         return V.CREATE_FORM
     }
@@ -32,15 +67,13 @@ internal class ManagerControllerImpl : ManagerController {
                         model: Model): String {
         if (log.isTraceEnabled)
             log.trace("args : req={}, result={}, model={}", req, result, model)
+        validate(req, result)
+        val template = if (result.hasErrors())
+            doCreateForm(model)
+        else
+            doCreate(req, result, model)
 
-        managerBorderline.create(CreateManagerCmd(
-                req.email!!,
-                req.name!!,
-                req.secret!!
-        ))
-
-        val template = "redirect:/"
-        if (log.isTraceEnabled) log.trace("result : model={}, template='$template'")
+        if (log.isTraceEnabled) log.trace("result : template='{}', model={}", template, model)
         return template
     }
 }
